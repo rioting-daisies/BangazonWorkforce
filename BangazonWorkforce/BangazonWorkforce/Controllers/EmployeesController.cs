d83b83a9cf27d60ef390cda0323609a5273ba566
Chris Morgan <bluemorgan2323@gmail.com>
2019-07-08 15:24:02 -0500

    Created TrainingProgram.cs in the Models directory. Implemented all the currently known properties into the model

--- BangazonWorkforce/BangazonWorkforce/Controllers/EmployeesController.cs
+++ BangazonWorkforce/BangazonWorkforce/Controllers/EmployeesController.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace BangazonWorkforce.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly IConfiguration _config;
+
+        public EmployeesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
         // GET: Employees
         public ActionResult Index()
         {
---
+++ BangazonWorkforce/BangazonWorkforce/Models/TrainingProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models
+{
+    public class TrainingProgram
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "You must provide a name for this training program.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must provide a start date for this training program.")]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "You must provide a end date for this training program.")]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        [Required(ErrorMessage = "You must provide the max attendee number for this training program.")]
+        [Display(Name = "Max Attendees")]
+        public int MaxAttendees { get; set; }
+    }
+}
