7854483a92546b97ed91172706830bb5f3652b67
greenskybluephish <greenskybluephish@gmail.com>
2019-07-08 15:59:03 -0500

    employee view model

---
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+
+        public List<SelectListItem> Departments { get; set; }
+        public Employee employee { get; set; }
+
+        private string _connectionString;
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+        public EmployeeCreateViewModel() { }
+
+        public EmployeeCreateViewModel(string connectionString)
+        {
+            _connectionString = connectionString;
+
+            Departments = GetAllDepartments()
+                .Select(d => new SelectListItem
+                {
+                    Text = d.Name,
+                    Value = d.Id.ToString()
+                })
+                .ToList();
+
+            Departments
+                .Insert(0, new SelectListItem
+                {
+                    Text = "Choose department...",
+                    Value = "0"
+                });
+
+        }
+
+        private List<Department> GetAllDepartments()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Department";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Department> departments = new List<Department>();
+                    while (reader.Read())
+                    {
+                        departments.Add(new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        });
+                    }
+
+                    reader.Close();
+
+                    return departments;
+                }
+            }
+        }
+    }
+}
