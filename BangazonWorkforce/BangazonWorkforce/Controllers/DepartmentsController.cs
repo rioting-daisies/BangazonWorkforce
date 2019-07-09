6598c3590f8466800d6303123234d94686a334ba
unknown <cliftonmatuszewski@gmail.com>
2019-07-09 10:19:08 -0500

    Checking a branch

--- BangazonWorkforce/BangazonWorkforce/Controllers/DepartmentsController.cs
+++ BangazonWorkforce/BangazonWorkforce/Controllers/DepartmentsController.cs
@@ -1,18 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonWorkforce.Models;
+using BangazonWorkforce.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
 
 namespace BangazonWorkforce.Controllers
 {
     public class DepartmentController : Controller
     {
+        private readonly IConfiguration _config;
+
+        public DepartmentController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
         // GET: Department
         public ActionResult Index()
         {
-            return View();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT d.Id, d.Name AS DepartmentName, d.Budget AS Money, 
+                                        COUNT(e.Id) AS EmployeeId
+                                        FROM Department d 
+                                        JOIN Employee e on d.Id = e.DepartmentId
+                                        GROUP BY d.Name, d.Budget, d.Id";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Department> departments = new List<Department>();
+                    while (reader.Read())
+                    {
+                        Department department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal(("Id"))),
+                            Name = reader.GetString(reader.GetOrdinal("DepartmentName")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
+                            EmployeeCount = reader.GetInt32(reader.GetOrdinal("EmployeeId"))
+                        };
+                        departments.Add(department);
+                    }
+                    reader.Close();
+
+                    return View(departments);
+                } 
+            }
+         
         }
 
         // GET: Department/Details/5
@@ -89,5 +137,35 @@ namespace BangazonWorkforce.Controllers
                 return View();
             }
         }
+        private Department GetDepartmentById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name, Budget FROM Department WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    Department department = null;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        department = new Department()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+                        };
+
+                    }
+                    reader.Close();
+                    return department;
+                }
+            }
+        }
+
     }
 }
